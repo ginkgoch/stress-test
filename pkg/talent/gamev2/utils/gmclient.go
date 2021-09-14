32c2901d14e3af0345e91bc83e1f2a95e2e95d2e
Howard Chen <howard.chen@moblab.com>
2021-09-14 17:47:58 +0800

    game client refactor

--- pkg/talent/gamev2/utils/gmclient.go
+++ pkg/talent/gamev2/utils/gmclient.go
@@ -1,8 +1,11 @@
 package utils
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"strconv"
 )
 
 type GameConfig struct {
@@ -31,23 +34,242 @@ func (g *GameClient) Close() error {
 	return g.WSClient.Close()
 }
 
-func (g *GameClient) PlayGame() (err error) {
-	defer g.Close()
+func (client *GameClient) PlayGame() (err error) {
+	defer client.Close()
 
 	log.Println("connecting")
-	if err = g.WSClient.Connect(); err != nil {
+	if err = client.WSClient.Connect(); err != nil {
 		return
 	}
 
 	log.Println("handshake")
-	if err = g.WSClient.Handshake(); err != nil {
+	if err = client.Handshake(); err != nil {
 		return
 	}
 
 	log.Println("heatbeat")
-	if err = g.WSClient.Heartbeat(); err != nil {
+	if err = client.Heartbeat(); err != nil {
 		return
 	}
 
 	return
+}
+
+// step 2, handshake
+func (client *GameClient) Handshake() (err error) {
+	var handshakeBody = `[
+		{
+			"id":"1",
+			"version":"1.0",
+			"minimumVersion":"1.0",
+			"channel":"/meta/handshake",
+			"supportedConnectionTypes":[
+				"websocket",
+				"long-polling",
+				"callback-polling"
+			],
+			"advice":{
+				"timeout":60000,
+				"interval":0
+			},
+			"ext":{
+				"ack":true
+			}
+		}
+	]`
+
+	err = client.WSClient.WriteTextMessage(handshakeBody)
+	if err != nil {
+		log.Println("handshake sending failed")
+		return
+	}
+
+	originMsg, handshakeMsg, err := client.WSClient.ReadMessage("handshake")
+	if err != nil {
+		return
+	}
+
+	err = ValidateChannel(handshakeMsg, "handshake", "/meta/handshake")
+	if err != nil {
+		return
+	}
+
+	var handshakeMsgs []HandshakeMsg
+	err = json.Unmarshal(originMsg, &handshakeMsgs)
+	if err != nil {
+		log.Println("handshakeMsg json err:", err, handshakeMsgs)
+		return
+	}
+
+	if len(handshakeMsgs) < 1 {
+		errMsg := "handshakeMsg json item count err"
+		log.Println(errMsg)
+		err = errors.New(errMsg)
+		return
+	}
+
+	client.WSClient.ClientId = handshakeMsgs[0].ClientID
+	return
+}
+
+// step 3, heartbeat
+func (client *GameClient) Heartbeat() (err error) {
+	connectMsg := &HeartbeatMsg{
+		ConnectionType: "websocket",
+		Ext:            map[string]interface{}{"ack": 0},
+		Channel:        "/meta/connect",
+	}
+
+	client.WSClient.MessageId++
+	connectMsg.ID = strconv.Itoa(client.WSClient.MessageId)
+	connectMsg.ClientID = client.WSClient.ClientId
+
+	err = client.WSClient.WriteJSON(connectMsg)
+	if err != nil {
+		log.Println("heartbeat sending failed")
+		return
+	}
+
+	originMsg, msg, err := client.WSClient.ReadMessage("connect")
+	if err != nil {
+		return
+	}
+
+	err = ValidateChannel(msg, "connect", "/meta/connect")
+	if err != nil {
+		return
+	}
+
+	var heartbeats []HeartbeatMsg
+	err = json.Unmarshal(originMsg, &heartbeats)
+	if err != nil {
+		log.Println("connect json err:", err, heartbeats)
+		return
+	}
+
+	client.WSClient.IncrementMessageId()
+	heartbeatBody := heartbeats[0]
+	heartbeatBody.ConnectionType = "websocket" //connectionType: 'websocket'
+	heartbeatBody.ID = strconv.Itoa(client.WSClient.MessageId)
+	err = client.WSClient.WriteJSON(heartbeatBody)
+	if err != nil {
+		log.Println("send heartbeat failed", err)
+		return
+	}
+
+	return
+}
+
+// joinOrLeave: join|leave
+func (client *GameClient) processGameJoinOrLeave(config *GameConfig, joinOrLeave string) (err error) {
+	joinGame := JoinGameMsg{
+		Action: joinOrLeave,
+		Room:   config.RoomID,
+		User:   config.PlayerID,
+	}
+
+	err = client.SendAction(joinGame, "/service/gameroom/"+config.RoomID)
+	if err != nil {
+		return
+	}
+
+	_, msg, err := client.WSClient.ReadMessage(joinOrLeave + "-game")
+	if err != nil {
+		return
+	}
+
+	err = ValidateChannel(msg, joinOrLeave+"-game", "/service/gameroom/"+config.RoomID)
+	if err != nil {
+		return
+	}
+
+	return err
+}
+
+// step 4, join game
+func (client *GameClient) JoinGame(config *GameConfig) (err error) {
+	err = client.processGameJoinOrLeave(config, "join")
+	return
+}
+
+// step 5, leave game
+func (client *GameClient) LeaveGame(config *GameConfig) (err error) {
+	err = client.processGameJoinOrLeave(config, "leave")
+	return
+}
+
+func (client *GameClient) SendAction(action interface{}, channel string) (err error) {
+	actionMsg := ActionMsg{
+		Data:    action,
+		Channel: channel,
+	}
+
+	client.WSClient.IncrementMessageId()
+	actionMsg.ID = strconv.Itoa(client.WSClient.MessageId)
+	err = client.WSClient.WriteJSON(actionMsg)
+	return
+}
+
+func (client *GameClient) AutoPlay() error {
+	for {
+		_, msg, err := client.WSClient.ReadMessage("game move")
+		if err != nil {
+			return err
+		}
+
+		data, err := json.Marshal(msg.Data)
+		if err != nil {
+			log.Println("game move msg format error")
+		}
+
+		receiveMsg := GameReceiveMsg{
+			Channel: msg.Channel,
+			Data:    data,
+		}
+
+		switch msg.Channel {
+		case "error":
+			err = errors.New(string(data))
+			return err
+		case "/gameroom":
+			event := new(GameEvent)
+			err = json.Unmarshal(receiveMsg.Data, event)
+			if err != nil {
+				log.Println("game room move json error", err)
+				return err
+			}
+
+			err = client.handleGameEvent(event, &receiveMsg)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (client *GameClient) handleGameEvent(event *GameEvent, receivedMsg *GameReceiveMsg) error {
+	switch event.Event {
+	case "UNAVAILABLE":
+		return errors.New("game UNAVAILABLE")
+	case "USER_JOINED":
+		return client.handleGameJoinedEvent(receivedMsg)
+	default:
+		log.Println("unknown game event", event.Event)
+		return nil
+	}
+}
+
+func (client *GameClient) handleGameJoinedEvent(receivedMsg *GameReceiveMsg) error {
+	joinedMsg := &GameJoinedMsg{}
+	err := json.Unmarshal(receivedMsg.Data, joinedMsg)
+	if err != nil {
+		log.Println("game joined msg parse failed")
+		return err
+	}
+	if !joinedMsg.Active {
+		return errors.New("join game ,not active")
+	}
+
+	//TODO: user joined
+	return nil
 }
--- pkg/talent/gamev2/utils/wsclient.go
+++ pkg/talent/gamev2/utils/wsclient.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
-	"strconv"
 
 	"github.com/gorilla/websocket"
 )
@@ -43,7 +42,7 @@ func (wsc *WSClient) Connect() (err error) {
 	return
 }
 
-func (wsc *WSClient) readMessage(action string) (originMsg []byte, msg ReceivedMsg, err error) {
+func (wsc *WSClient) ReadMessage(action string) (originMsg []byte, msg ReceivedMsg, err error) {
 	_, originMsg, err = wsc.Conn.ReadMessage()
 
 	if err != nil {
@@ -76,7 +75,19 @@ func (wsc *WSClient) readMessage(action string) (originMsg []byte, msg ReceivedM
 	return
 }
 
-func validateChannel(msg ReceivedMsg, action string, channel string) (err error) {
+func (wsc *WSClient) WriteTextMessage(text string) error {
+	return wsc.Conn.WriteMessage(websocket.TextMessage, []byte(text))
+}
+
+func (wsc *WSClient) WriteJSON(content interface{}) error {
+	return wsc.Conn.WriteJSON([]interface{}{content})
+}
+
+func (wsc *WSClient) IncrementMessageId() {
+	wsc.MessageId++
+}
+
+func ValidateChannel(msg ReceivedMsg, action string, channel string) (err error) {
 	if msg.Channel != channel {
 		errMsg := action + " receive channel incorrect: " + msg.Channel
 		log.Println(errMsg)
@@ -84,222 +95,4 @@ func validateChannel(msg ReceivedMsg, action string, channel string) (err error)
 	}
 
 	return
-}
-
-// step 2, handshake
-func (wsc *WSClient) Handshake() (err error) {
-	var handshakeBody = `[
-		{
-			"id":"1",
-			"version":"1.0",
-			"minimumVersion":"1.0",
-			"channel":"/meta/handshake",
-			"supportedConnectionTypes":[
-				"websocket",
-				"long-polling",
-				"callback-polling"
-			],
-			"advice":{
-				"timeout":60000,
-				"interval":0
-			},
-			"ext":{
-				"ack":true
-			}
-		}
-	]`
-
-	err = wsc.Conn.WriteMessage(websocket.TextMessage, []byte(handshakeBody))
-	if err != nil {
-		log.Println("handshake sending failed")
-		return
-	}
-
-	originMsg, handshakeMsg, err := wsc.readMessage("handshake")
-	if err != nil {
-		return
-	}
-
-	err = validateChannel(handshakeMsg, "handshake", "/meta/handshake")
-	if err != nil {
-		return
-	}
-
-	var handshakeMsgs []HandshakeMsg
-	err = json.Unmarshal(originMsg, &handshakeMsgs)
-	if err != nil {
-		log.Println("handshakeMsg json err:", err, handshakeMsgs)
-		return
-	}
-
-	if len(handshakeMsgs) < 1 {
-		errMsg := "handshakeMsg json item count err"
-		log.Println(errMsg)
-		err = errors.New(errMsg)
-		return
-	}
-
-	wsc.ClientId = handshakeMsgs[0].ClientID
-	return
-}
-
-// step 3, heartbeat
-func (wsc *WSClient) Heartbeat() (err error) {
-	connectMsg := &HeartbeatMsg{
-		ConnectionType: "websocket",
-		Ext:            map[string]interface{}{"ack": 0},
-		Channel:        "/meta/connect",
-	}
-
-	wsc.MessageId++
-	connectMsg.ID = strconv.Itoa(wsc.MessageId)
-	connectMsg.ClientID = wsc.ClientId
-
-	err = wsc.Conn.WriteJSON([]interface{}{connectMsg})
-	if err != nil {
-		log.Println("heartbeat sending failed")
-		return
-	}
-
-	originMsg, msg, err := wsc.readMessage("connect")
-	if err != nil {
-		return
-	}
-
-	err = validateChannel(msg, "connect", "/meta/connect")
-	if err != nil {
-		return
-	}
-
-	var heartbeats []HeartbeatMsg
-	err = json.Unmarshal(originMsg, &heartbeats)
-	if err != nil {
-		log.Println("connect json err:", err, heartbeats)
-		return
-	}
-
-	wsc.MessageId++
-	heartbeatBody := heartbeats[0]
-	heartbeatBody.ConnectionType = "websocket" //connectionType: 'websocket'
-	heartbeatBody.ID = strconv.Itoa(wsc.MessageId)
-	err = wsc.Conn.WriteJSON([]interface{}{heartbeatBody})
-	if err != nil {
-		log.Println("send heartbeat failed", err)
-		return
-	}
-
-	return
-}
-
-// joinOrLeave: join|leave
-func (wsc *WSClient) processGameJoinOrLeave(config *GameConfig, joinOrLeave string) (err error) {
-	joinGame := JoinGameMsg{
-		Action: joinOrLeave,
-		Room:   config.RoomID,
-		User:   config.PlayerID,
-	}
-
-	err = wsc.SendAction(joinGame, "/service/gameroom/"+config.RoomID)
-	if err != nil {
-		return
-	}
-
-	_, msg, err := wsc.readMessage(joinOrLeave + "-game")
-	if err != nil {
-		return
-	}
-
-	err = validateChannel(msg, joinOrLeave+"-game", "/service/gameroom/"+config.RoomID)
-	if err != nil {
-		return
-	}
-
-	return err
-}
-
-// step 4, join game
-func (wsc *WSClient) JoinGame(config *GameConfig) (err error) {
-	err = wsc.processGameJoinOrLeave(config, "join")
-	return
-}
-
-// step 5, leave game
-func (wsc *WSClient) LeaveGame(config *GameConfig) (err error) {
-	err = wsc.processGameJoinOrLeave(config, "leave")
-	return
-}
-
-func (wsc *WSClient) SendAction(action interface{}, channel string) (err error) {
-	actionMsg := ActionMsg{
-		Data:    action,
-		Channel: channel,
-	}
-
-	wsc.MessageId++
-	actionMsg.ID = strconv.Itoa(wsc.MessageId)
-	err = wsc.Conn.WriteJSON([]interface{}{actionMsg})
-	return
-}
-
-func (wsc *WSClient) AutoPlay() error {
-	for {
-		_, msg, err := wsc.readMessage("game move")
-		if err != nil {
-			return err
-		}
-
-		data, err := json.Marshal(msg.Data)
-		if err != nil {
-			log.Println("game move msg format error")
-		}
-
-		receiveMsg := GameReceiveMsg{
-			Channel: msg.Channel,
-			Data:    data,
-		}
-
-		switch msg.Channel {
-		case "error":
-			err = errors.New(string(data))
-			return err
-		case "/gameroom":
-			event := new(GameEvent)
-			err = json.Unmarshal(receiveMsg.Data, event)
-			if err != nil {
-				log.Println("game room move json error", err)
-				return err
-			}
-
-			err = wsc.handleGameEvent(event, &receiveMsg)
-			if err != nil {
-				return err
-			}
-		}
-	}
-}
-
-func (wsc *WSClient) handleGameEvent(event *GameEvent, receivedMsg *GameReceiveMsg) error {
-	switch event.Event {
-	case "UNAVAILABLE":
-		return errors.New("game UNAVAILABLE")
-	case "USER_JOINED":
-		return wsc.handleGameJoinedEvent(receivedMsg)
-	default:
-		log.Println("unknown game event", event.Event)
-		return nil
-	}
-}
-
-func (wsc *WSClient) handleGameJoinedEvent(receivedMsg *GameReceiveMsg) error {
-	joinedMsg := &GameJoinedMsg{}
-	err := json.Unmarshal(receivedMsg.Data, joinedMsg)
-	if err != nil {
-		log.Println("game joined msg parse failed")
-		return err
-	}
-	if !joinedMsg.Active {
-		return errors.New("join game ,not active")
-	}
-
-	return nil
 }
